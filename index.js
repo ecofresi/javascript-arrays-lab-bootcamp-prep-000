b379b9ff2394dd98c243199f15215f5640788dea
Ethel Cofresi <cofresi.ethel@gmail.com>
2018-01-27 02:44:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,21 @@ function destructivelyPrependKitten(name){
      return kittens.shift();
  }
  
+  function appendKitten(name){
+    
+     return [...kittens, name];
+ }
  
+ function prependKitten(name){
+   
+     return [name, ...kittens];
+ }
+ 
+ function removeLastKitten(){
+   
+     return kittens.slice(0,kittens.length-1);
+ }
+ 
+ function removeFirstKitten(){
+     return kittens.slice(1);
+ }
